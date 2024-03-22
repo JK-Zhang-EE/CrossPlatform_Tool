67120f4c05cde46e674891e5128600357ac7a6da
kai.chang <kai@kaichangs-MacBook-Pro.local>
2024-03-22 16:55:36 +0800

    remove file

--- build/CMakeFiles/MyTool.dir/src/math/MyMath.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/MyTool.dir/src/math/MyMath.cpp.o: \
-  /Users/kai/Desktop/Project/CrossPlatform_Tool/src/math/MyMath.cpp \
-  /Users/kai/Desktop/Project/CrossPlatform_Tool/include/math/MyMath.h
