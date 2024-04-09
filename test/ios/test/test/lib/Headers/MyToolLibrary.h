3e6689b6ff43850642ee5833da54a6b0344b97b5
kai.chang <kai@kaichangs-MacBook-Pro.local>
2024-04-09 11:30:18 +0800

    update ios lib

---
+++ test/ios/test/test/lib/Headers/MyToolLibrary.h
@@ -0,0 +1,12 @@
+// MyToolLibrary.h
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface MyToolLibrary : NSObject
+
+- (int)getMathAddResult:(int)x y:(int)y;
+
+- (int)getMathSubResult:(int)x y:(int)y;
+
+@end
